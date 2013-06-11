41dad54750115b6ac95a0e211d211d3243f88df7
Dale Stammen <stammen@gmail.com>
2013-06-11 16:34:36 -0700

    commented out unused key command string

--- Box2D/Testbed/Tests/OneSidedPlatform.h
+++ Box2D/Testbed/Tests/OneSidedPlatform.h
@@ -110,8 +110,8 @@ public:
 	void Step(Settings* settings)
 	{
 		Test::Step(settings);
-		m_debugDraw.DrawString(5, m_textLine, "Press: (c) create a shape, (d) destroy a shape.");
-		m_textLine += DRAW_STRING_NEW_LINE;
+		//m_debugDraw.DrawString(5, m_textLine, "Press: (c) create a shape, (d) destroy a shape.");
+		//m_textLine += DRAW_STRING_NEW_LINE;
 
         b2Vec2 v = m_character->GetBody()->GetLinearVelocity();
         m_debugDraw.DrawString(5, m_textLine, "Character Linear Velocity: %f", v.y);
